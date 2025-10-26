a49cb25b208ff8304337d3d6d991a523c330de21
Peter Forret <peter@forret.com>
2025-10-26 15:08:18 +0100

    MOD: bash_unit.yml test_basic.sh

--- tests/test_basic.sh
+++ tests/test_basic.sh
@@ -13,7 +13,7 @@
 root_folder=$(cd .. && pwd) # tests/.. is root folder
 # shellcheck disable=SC2012
 # shellcheck disable=SC2035
-root_script=$(find "$root_folder"  -maxdepth 1 -name "*.sh" | head -1) # normally there should be only 1
+root_script=phackup.sh
 
 test_no_parameters_shows_usage() {
   # script without parameters should give usage info
@@ -23,5 +23,5 @@ test_no_parameters_shows_usage() {
 test_usage_shows_option_verbose() {
   # script without parameters should show option -v or --verbose
   assert_equals 1 "$("$root_script" 2>&1 | grep -c "Usage")"
-  assert_equals 1 "$("$root_script" 2>&1 | grep -c "verbose")"
+  assert_equals 1 "$("$root_script" 2>&1 | grep -c "VERBOSE")"
 }
